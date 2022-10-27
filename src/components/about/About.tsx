030930a3db18ae71be6c3413a921b58ca2816d65
adityavag <104594678+adityavag@users.noreply.github.com>
2022-10-28 00:25:13 +0530

    Added About and Perks Component

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="z-10 w-full h-screen backdrop-blur-sm bg-gradient-to-b from-transparent to-black/80">
-      About
+    <div className="z-10 w-full h-screen backdrop-blur-sm bg-black/80">
+      <h2 className='font-["Orbitron"] font-[500] text-[40px] leading-[50.16px] text-center'>About the Event</h2>
+      <p className='font-[Raleway] font-[500] text-[16px] leading-[18.78px] mt-[80px] text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ipsa maxime quo alias vero consectetur? Iure mollitia consequuntur illum sed inventore modi! Ipsum exercitationem vitae quasi voluptatem iure esse aliquam.</p>
     </div>
   );
 };
