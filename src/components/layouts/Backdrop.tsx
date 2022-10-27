a6eb997e5caec394d1bb8f5c2ce810f31f38ec6d
kunalkeshan <kunalkeshan12@gmail.com>
2022-10-27 18:19:24 +0530

    fix: use video instead of gif (use muted attr to autoplay video)

--- src/components/layouts/Backdrop.tsx
+++ src/components/layouts/Backdrop.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-// import BackgroundVideo from '../../assets/background/background.mp4';
-import BackgroundVideo from '../../assets/background/background.gif';
+import BackgroundVideo from '../../assets/background/background.mp4';
+// import BackgroundVideo from '../../assets/background/background.gif';
 
 const Backdrop = () => {
 	return (
 		<div className='fixed top-0 left-0 w-full h-screen -z-10'>
-			{/* <video className='w-full h-full object-cover' autoPlay loop>
+			<video className='w-full h-full object-cover' autoPlay loop muted>
 				<source src={BackgroundVideo} type='video/mp4' />
-			</video> */}
-			<img
+			</video>
+			{/* <img
 				src={BackgroundVideo}
 				alt='Hello, Cloud backdrop'
 				className='w-full h-full object-cover -z-10'
-			/>
+			/> */}
 		</div>
 	);
 };
