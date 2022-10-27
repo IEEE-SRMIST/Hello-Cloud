022dae1bb18abc181ad8941ec8bc30538d4fc86d
Snehil <snehilwebber@gmail.com>
2022-10-27 19:24:38 +0530

    chore: add prettier config

--- src/components/speakers/Speakers.tsx
+++ src/components/speakers/Speakers.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Speakers = () => {
-	return (
-		<div className='z-10 w-full h-screen backdrop-blur-sm bg-black/80'>
-			Speakers
-		</div>
-	);
+  return (
+    <div className="z-10 w-full h-screen backdrop-blur-sm bg-black/80">
+      Speakers
+    </div>
+  );
 };
 
 export default Speakers;
--- src/components/sponsors/Sponsors.tsx
+++ src/components/sponsors/Sponsors.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Sponsors = () => {
-	return (
-		<div className='z-10 w-full h-screen backdrop-blur-sm bg-black/80'>
-			Sponsors
-		</div>
-	);
+  return (
+    <div className="z-10 w-full h-screen backdrop-blur-sm bg-black/80">
+      Sponsors
+    </div>
+  );
 };
 
 export default Sponsors;
