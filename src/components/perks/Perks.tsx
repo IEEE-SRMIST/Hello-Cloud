9797ba3c9bf7a4f1e41e94a2956b00e6dfd644f7
kunalkeshan <kunalkeshan12@gmail.com>
2022-10-28 14:57:43 +0530

    fix: background gradient and padding

--- src/components/perks/Perks.tsx
+++ src/components/perks/Perks.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 const Perks = () => {
   return (
-    <div className="z-10 w-full h-screen backdrop-blur-sm bg-black/80">
-      <h2 className='font-["Orbitron"] font-[500] text-[40px] leading-[50.16px] text-center'>What's in it for you?</h2>
-      <p className='font-[Raleway] font-[500] text-[16px] leading-[18.78px] mt-[80px] text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ipsa maxime quo alias vero consectetur? Iure mollitia consequuntur illum sed inventore modi! Ipsum exercitationem vitae quasi voluptatem iure esse aliquam.</p>
+    <div className="z-10 w-full h-screen backdrop-blur-sm bg-black/80 px-8 md:px-20">
+      <h2 className='font-["Orbitron"] font-[500] text-[40px] leading-[50.16px] text-center'>
+        What's in it for you?
+      </h2>
+      <p className="font-[Raleway] font-[500] text-[16px] leading-[18.78px] mt-[80px] text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        ipsa maxime quo alias vero consectetur? Iure mollitia consequuntur illum
+        sed inventore modi! Ipsum exercitationem vitae quasi voluptatem iure
+        esse aliquam.
+      </p>
     </div>
   );
 };
