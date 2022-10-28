e93c6adf03818efb44a9473144b899c556923421
kunalkeshan <kunalkeshan12@gmail.com>
2022-10-28 15:02:35 +0530

    fix: arbitrary values replaced with tailwind classes

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 const About = () => {
   return (
     <div className="z-10 w-full h-screen  bg-gradient-to-b from-transparent backdrop-blur-sm to-black/80 px-8 md:px-20">
-      <h2 className='font-["Orbitron"] font-[500] text-[40px] leading-[50.16px] text-center'>
+      <h2 className='font-["Orbitron"] font-semibold text-[40px] leading-[50.16px] text-center'>
         About the Event
       </h2>
-      <p className="font-[Raleway] font-[500] text-[16px] leading-[18.78px] mt-[80px] text-center">
+      <p className="font-[Raleway] font-semibold text-[16px] leading-[18.78px] mt-[80px] text-center">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
         ipsa maxime quo alias vero consectetur? Iure mollitia consequuntur illum
         sed inventore modi! Ipsum exercitationem vitae quasi voluptatem iure
