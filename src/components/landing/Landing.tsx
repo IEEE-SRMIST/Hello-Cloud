795434fcdf76678d98370825bdcd51d96e2eeff4
Arnav Shukla <87523233+thelordzeus@users.noreply.github.com>
2022-10-28 23:23:30 +0530

    added navbar and landing page (#6)
    * added navbar and landing page
    merged
    
    merged
    
    * completed changed

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const Landing = () => {
-  return <div className="z-10 w-full h-screen">Landing</div>;
+  return (
+    <div className="z-10  h-screen ">
+      <div className="md:mt-42 md:ml-44 mt-32 ml-9 ">
+        <div>
+          <h1 className='md:text-7xl font-["Orbitron"] text-5xl '>
+            Hello, Cloud!
+          </h1>
+        </div>
+        <div className="mt-2">
+          <button className="bg-white hover:bg-gray-300 border rounded-full px-4 py-2">
+            <h1 className="text-2xl text-black">Register</h1>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,7 +1,97 @@
-import React from 'react';
+// import React from 'react';
+import Logo from '../../assets/img/ieee.png';
+import sponser from '../../assets/img/salesForce-removedbg.png';
+import React, { useState, useEffect } from 'react';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [nav, setNav] = useState(false);
+  const [color, setColor] = useState('transparent');
+  const [textColor, setTextColor] = useState('white');
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  useEffect(() => {
+    const changeColor = () => {
+      if (window.scrollY >= 90) {
+        setColor('transparent');
+        setTextColor('#ffffff');
+      } else {
+        setColor('transparent');
+        setTextColor('#ffffff');
+      }
+    };
+    window.addEventListener('scroll', changeColor);
+  }, []);
+
+  return (
+    <div
+      style={{ backgroundColor: `${color}` }}
+      className=" left-0 top-0 w-full z-10 ease-in duration-300"
+    >
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
+        <div className="space-x-2">
+          <img src={Logo} alt="IEEE Logo" className="md:h-36 h-24 inline" />
+          <img
+            src={sponser}
+            alt="Sales Force logo"
+            className="md:h-36 h-24 inline "
+          />
+        </div>
+        <ul style={{ color: `${textColor}` }} className="hidden md:flex">
+          <li className="p-3 text-2xl hover:text-blue-600">
+            <a href="#">About Us</a>
+          </li>
+          <li className="p-3 text-2xl hover:text-blue-600">
+            <a href="#">About the Event</a>
+          </li>
+          <li className="p-3 text-2xl hover:text-blue-600">
+            <a href="#">Sponsors</a>
+          </li>
+        </ul>
+
+        {/* Mobile Button */}
+        <div onClick={handleNav} className="block md:hidden z-10">
+          {nav ? (
+            <AiOutlineClose size={20} style={{ color: `${textColor}` }} />
+          ) : (
+            <AiOutlineMenu size={20} style={{ color: `${textColor}` }} />
+          )}
+        </div>
+        {/* Mobile Menu */}
+        <div
+          className={
+            nav
+              ? 'md:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+              : 'md:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+          }
+        >
+          <ul>
+            <li
+              onClick={handleNav}
+              className="p-4 text-4xl hover:text-blue-600"
+            >
+              <a href="/">About Us</a>
+            </li>
+            <li
+              onClick={handleNav}
+              className="p-4 text-4xl hover:text-blue-600"
+            >
+              <a href="/#gallery">About the Event</a>
+            </li>
+            <li
+              onClick={handleNav}
+              className="p-4 text-4xl hover:text-blue-600"
+            >
+              <a href="/work">Sponsors</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
