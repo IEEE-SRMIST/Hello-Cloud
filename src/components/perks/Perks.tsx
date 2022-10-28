1955c46d75821ae39f29984e44301a5406fb448b
Aditya Vardhan Agarwal <104594678+adityavag@users.noreply.github.com>
2022-10-28 17:04:58 +0530

    Added About and Perks Component (#5)
    Co-authored-by: kunalkeshan <kunalkeshan12@gmail.com>

--- src/components/perks/Perks.tsx
+++ src/components/perks/Perks.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const Perks = () => {
   return (
-    <div className="z-10 w-full h-screen backdrop-blur-sm bg-black/80">
-      Perks
+    <div className="z-10 w-full h-screen backdrop-blur-sm bg-black/80 px-8 md:px-20">
+      <h2 className='font-["Orbitron"] font-semibold text-[40px] leading-[50.16px] text-center'>
+        What's in it for you?
+      </h2>
+      <p className="font-[Raleway] font-semibold text-[16px] leading-[18.78px] mt-[80px] text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        ipsa maxime quo alias vero consectetur? Iure mollitia consequuntur illum
+        sed inventore modi! Ipsum exercitationem vitae quasi voluptatem iure
+        esse aliquam.
+      </p>
     </div>
   );
 };
