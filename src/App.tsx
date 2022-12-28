f0b0b523b37309a8156d128679c6aa2aea17898e
Kunal Keshan <kunalkeshan12@gmail.com>
2022-12-28 23:32:55 +0530

    fix: unused vars commented out

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,25 @@ import React from 'react';
 import About from './components/about/About';
 import Landing from './components/landing/Landing';
 import Backdrop from './components/layouts/Backdrop';
-import Footer from './components/layouts/Footer';
+// import Footer from './components/layouts/Footer';
 import Navbar from './components/layouts/Navbar';
 import Perks from './components/perks/Perks';
 import Speakers from './components/speakers/Speakers';
 import Sponsors from './components/sponsors/Sponsors';
 
 function App() {
-	return (
-		<div className='w-full h-screen relative'>
-			<Navbar />
-			<Backdrop />
-			<Landing />
-			<About />
-			<Perks />
-			<Sponsors />
-			<Speakers />
-			{/* <Footer /> */}
-		</div>
-	);
+  return (
+    <div className="w-full h-screen relative">
+      <Navbar />
+      <Backdrop />
+      <Landing />
+      <About />
+      <Perks />
+      <Sponsors />
+      <Speakers />
+      {/* <Footer /> */}
+    </div>
+  );
 }
 
 export default App;
