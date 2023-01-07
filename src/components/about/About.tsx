5925471528a6c45a62368b2b45ad99a618965b27
Kunal Keshan <kunalkeshan12@gmail.com>
2023-01-07 19:23:08 +0530

    chore: updated venue and time in about

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 
 const About = () => {
-  return (
-    <div
-      className="z-10 w-full pb-40 px-8 md:px-20 max-w-6xl mx-auto"
-      id="about"
-    >
-      <h2 className='font-["Orbitron"] font-semibold text-[40px] leading-[50.16px] text-center'>
-        About the Event
-      </h2>
-      <p className="font-[Raleway] text-[16px] leading-[24px] mt-[80px] text-center">
-        Eager to delve into the world of cloud computing? Well, “Hello, Cloud”
-        has got it all covered for you, jumping into a wide variety of topics
-        that will get your gears running. Get acquainted with the industry
-        techniques in the area of cloud computing, learn build highly scalable
-        systems, handle security, and more at "Hello, Cloud!" at MBA Hall, SRM Kattankulathur Campus on 10ᵗʰ November, 2022
-      </p>
-    </div>
-  );
+	return (
+		<div
+			className='z-10 w-full pb-40 px-8 md:px-20 max-w-6xl mx-auto'
+			id='about'
+		>
+			<h2 className='font-["Orbitron"] font-semibold text-[40px] leading-[50.16px] text-center'>
+				About the Event
+			</h2>
+			<p className='font-[Raleway] text-[16px] leading-[24px] mt-[80px] text-center'>
+				Eager to delve into the world of cloud computing? Well, “Hello,
+				Cloud” has got it all covered for you, jumping into a wide
+				variety of topics that will get your gears running. Get
+				acquainted with the industry techniques in the area of cloud
+				computing, learn build highly scalable systems, handle security,
+				and more at "Hello, Cloud!" at Mini-Hall 2, T.P. Ganesan
+				Auditorium, SRM Kattankulathur on 28ᵗʰ January, 2023 at 10 A.M.
+			</p>
+		</div>
+	);
 };
 
 export default About;
