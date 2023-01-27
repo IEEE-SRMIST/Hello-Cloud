d01222925bc49823de77443cc41bb09e2bab0166
Rushit Patel <rushitpatel@Rushits-MacBook-Air.local>
2023-01-27 22:33:29 +0530

    Pic Not showing

--- src/components/speakers/Speakers.tsx
+++ src/components/speakers/Speakers.tsx
@@ -1,48 +1,25 @@
 import React from 'react';
-import KK from '../../assets/img/kkpic-removedbg-.png';
 import Footer from '../layouts/Footer';
-import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
-import { FaGlobe } from 'react-icons/fa';
+import speakers from './data';
+import SpeakerInfo from './Tamplet'
 
 const Speakers = () => {
-  const socials = [
-    {
-      Icon: AiFillGithub,
-      url: 'https://github.com/CodeBuildder',
-    },
-    {
-      Icon: AiFillLinkedin,
-      url: 'https://www.linkedin.com/in/kaushikkums/',
-    },
-    {
-      Icon: FaGlobe,
-      url: 'https://kaushiks.xyz/',
-    },
-  ];
-
-  const handleOpenUrl = (url: string) => () => {
-    window.open(url, '_blank');
-  };
-
+  const spokeperson = speakers.map(info =>{
+    return <SpeakerInfo
+              name = {info.name}
+              image = {info.image}
+              des1 = {info.des1}
+              des2 = {info.des2}
+              github = {info.github}
+              linkedin = {info.linkedin}
+              portfolio = {info.portfolio}
+            />
+})
   return (
     <div className="flex flex-col items-center z-10 w-full">
       <div className='text-[40px] font-["Orbitron"] mt-16 mb-10'>Speaker</div>
-      <img className="h-60 w-60 mt-10" src={KK} alt="KK dp" />
-      <div className='text-[32px] font-["Lato"] my-8'>Kaushik Kumaran</div>
-      <div className='text-[16px] font-["Raleway"]'>
-        Cloud Engineer at Searce Inc
-      </div>
-      <div className='text-[16px] font-["Raleway"] mb-8'>
-        Webmaster at IEEE SRM WAD
-      </div>
-      <div className="flex mb-20 xl:mb-0 lg:pb-8 text-4xl gap-8">
-        {socials.map(({ Icon, url }, index) => (
-          <Icon
-            className="hover:cursor-pointer"
-            onClick={handleOpenUrl(url)}
-            key={index}
-          />
-        ))}
+      <div className='flex flex-col item-center justify-around md:flex-row'>
+      {spokeperson}
       </div>
       <Footer />
     </div>
---
+++ src/components/speakers/data.ts
@@ -0,0 +1,31 @@
+type myType ={
+    name:string,
+    image : string,
+    des1 : string,
+    des2: string,
+    github : string,
+    linkedin : string,
+    portfolio : string,
+}
+const speakers:myType[] = [
+    {
+        name : 'Kaushik Kumaran',
+        image : '../../assets/img/kkpic-removedbg-.png',
+        des1 : 'Cloud Engineer at Searce Inc',
+        des2 : 'Webmaster at IEEE SRM WAD',
+        github : 'https://github.com/CodeBuildder',
+        linkedin : 'https://www.linkedin.com/in/kaushikkums/',
+        portfolio : 'https://kaushiks.xyz/'
+    },
+    {
+        name : 'Kunal Keshan',
+        image : '../../assets/img/KunalBhaiya.png',
+        des1 : 'Co-Founder at Codelance Devs',
+        des2 : 'Acting Heat at IEEE SRM Computer Society',
+        github : 'https://GitHub.com/kunalkeshan',
+        linkedin : 'https://www.linkedin.com/in/kunalkeshan',
+        portfolio : 'https://kunalkeshan.dev'
+    }
+]
+
+export default speakers;
